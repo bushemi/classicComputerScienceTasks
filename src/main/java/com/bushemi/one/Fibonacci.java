70ad336d790cbc547dffab3b88d7b7cd44a0a3c1
IgorBoldysh <bushemi2001@gmail.com>
2022-02-13 07:12:30 +0200

    refactor: Refactor N-th fibonacci number advanced calculation.

--- src/main/java/com/bushemi/one/Fibonacci.java
+++ src/main/java/com/bushemi/one/Fibonacci.java
@@ -13,11 +13,8 @@ public class Fibonacci {
     }
 
     public int calculateNthNumberAdvanced(int n) {
-        if (!memo.containsKey(n)) {
-            memo.put(n, (calculateNthNumberAdvanced(n - 2) + calculateNthNumberAdvanced(n - 1)));
-        }
-        return memo.get(n);
-
+        return memo.computeIfAbsent(n,
+                key -> (calculateNthNumberAdvanced(n - 2) + calculateNthNumberAdvanced(n - 1)));
     }
 
 
