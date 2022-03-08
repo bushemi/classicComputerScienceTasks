ed6cfa48b1d4520bcac16ea0d37373d7291045c6
IgorBoldysh <bushemi2001@gmail.com>
2022-03-08 21:55:47 +0200

    fix: Add calculating hanoi tower problem

---
+++ src/main/java/com/bushemi/five/Hanoi.java
@@ -0,0 +1,32 @@
+package com.bushemi.five;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Hanoi {
+    public final Deque<Integer> towerA = new LinkedList<>();
+    public final Deque<Integer> towerB = new LinkedList<>();
+    public final Deque<Integer> towerC = new LinkedList<>();
+    private final int numDiscs;
+
+    public Hanoi(int discs) {
+        numDiscs = discs;
+        for (int i = 1; i <= discs; i++) {
+            towerA.add(i);
+        }
+    }
+
+    private void move(Deque<Integer> begin, Deque<Integer> end, Deque<Integer> temp, int n) {
+        if (n == 1) {
+            end.push(begin.pop());
+        } else {
+            move(begin, temp, end, n - 1);
+            move(begin, end, temp, 1);
+            move(temp, end, begin, n - 1);
+        }
+    }
+
+    public void solve() {
+        move(towerA, towerC, towerB, numDiscs);
+    }
+}
---
+++ src/test/java/com/bushemi/five/HanoiTest.java
@@ -0,0 +1,35 @@
+package com.bushemi.five;
+
+import org.testng.annotations.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class HanoiTest {
+
+    @Test
+    public void should_resolve_hanoi() {
+        //given
+        Hanoi towers = new Hanoi(3);
+        List<Integer> expectedTower = new LinkedList<>(asList(1, 2, 3));
+        assertThat(towers.towerA.size(), is(equalTo(3)));
+        assertThat(towers.towerB.size(), is(equalTo(0)));
+        assertThat(towers.towerC.size(), is(equalTo(0)));
+        assertThat(towers.towerA, is(equalTo(expectedTower)));
+
+        //when
+        towers.solve();
+
+        //then
+        assertThat(towers.towerA.size(), is(equalTo(0)));
+        assertThat(towers.towerB.size(), is(equalTo(0)));
+        assertThat(towers.towerC.size(), is(equalTo(3)));
+        assertThat(towers.towerC, is(equalTo(expectedTower)));
+    }
+
+}
