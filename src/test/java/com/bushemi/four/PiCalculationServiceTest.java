0886e0dbad0a24d3a6abc83029d4b2331b9fd0ba
IgorBoldysh <bushemi2001@gmail.com>
2022-03-08 21:09:52 +0200

    fix: Add test for calculating of Pi number

--- src/test/java/com/bushemi/four/PiCalculationServiceTest.java
+++ src/test/java/com/bushemi/four/PiCalculationServiceTest.java
@@ -2,6 +2,10 @@ package com.bushemi.four;
 
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class PiCalculationServiceTest {
     private final PiCalculationService service = new PiCalculationService();
 
@@ -9,9 +13,10 @@ public class PiCalculationServiceTest {
     public void should_calculate_pi() {
         //given
         //when
-        double pi = service.calculatePi(10000);
-        System.out.println("pi = " + pi);
+        double pi = service.calculatePi(100000);
+
         //then
+        assertThat(String.valueOf(pi).substring(0, 4), is(equalTo("3.14")));
     }
 
 }
